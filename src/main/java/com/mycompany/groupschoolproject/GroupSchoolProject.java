1c0b6738fb2cbb9b5002d6e7047e3992b31c8bba
aimad469 <aimad.bast@gmail.com>
2026-05-21 21:38:50 +0100

    CA2 commit

---
+++ src/main/java/com/mycompany/groupschoolproject/GroupSchoolProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.groupschoolproject;
+
+/**
+ *
+ * @author aimad
+ */
+public class GroupSchoolProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
